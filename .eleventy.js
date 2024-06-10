46c79ad15d21c3cec580926fa92b0a984d075cbe
Miguel Espinosa <miguel@Mac-mini-de-Miguel.local>
2024-06-10 14:03:24 -0600

    pass de toml a través de config eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -89,6 +89,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/styles');
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addPassthroughCopy('./src/netlify.toml');
   
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
