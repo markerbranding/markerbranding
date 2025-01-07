c5076f18c88acdb2e24be82aaa57082aa8487626
Miguel Espinosa <miguel@Mac-mini-de-Miguel.local>
2025-01-07 02:47:41 -0600

    vid altozano y loader

--- src/assets/scripts/loader.js
+++ src/assets/scripts/loader.js
@@ -1,53 +1,3 @@
-/*;(function(){
-	function id(v){return document.getElementById(v); }
-	function loadbar() {
-    var ovrl = id("overlay"),
-    log = id("logoloader"),
-    prog = id("progress"),
-    bgbar = id("progress__bg"),
-    stat = id("progstat"),
-    img = document.images,
-    c = 0;
-    tot = img.length;
-
-		function imgLoaded(){
-			c += 1;
-			var perc = ((100/tot*c) << 0) +"%";
-			prog.style.width = perc;
-			stat.innerHTML = ""+ perc;
-			if(c===tot) return doneLoading();
-		}
-		function doneLoading(){
-			ovrl.style.opacity = 0;
-			setTimeout(function(){ 
-				ovrl.style.display = "none";
-				var tlLoad = gsap.timeline();
-
-                tlLoad.to(".inner__transition", {
-                    delay: 0.3,
-					scaleY: 0,
-                    ease: "power4.out",
-                    duration: 0.5
-                })
-
-                tlLoad.to(".transition", {
-                    delay: 0,
-					scaleY: 0,
-                    ease: "power4.out",
-                    duration: 0.7
-                }, "-=0.3");
-			}, 500);
-		}
-		for(var i=0; i<tot; i++) {
-			var tImg     = new Image();
-			tImg.onload  = imgLoaded;
-			tImg.onerror = imgLoaded;
-			tImg.src     = img[i].src;
-		}    
-	}
-	document.addEventListener('DOMContentLoaded', loadbar, false);
-}());*/
-
 ;(function(){
 	function id(v){return document.getElementById(v); }
 	function loadbar() {
@@ -71,9 +21,9 @@
 			ovrl.style.opacity = 0;
 			setTimeout(function(){ 
 				ovrl.style.display = "none";
-				gsap.to(".transition__top", { height: "0%", ease: "power1.out", duration: 0.7 });
-				gsap.to(".transition__bottom", { height: "0%", ease: "power1.out", duration: 0.7 });
-			}, 500);/**/
+				gsap.to(".transition__top", { height: "0%", ease: "power1.in", duration: 0.4 });
+				gsap.to(".transition__bottom", { height: "0%", ease: "power1.in", duration: 0.4 });
+			}, 400);/**/
 		}
 		for(var i=0; i<tot; i++) {
 			var tImg     = new Image();
